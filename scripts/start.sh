3b50c1365a52428c8fb70dba7fb19980aa5a2db0
Andrew Hill <andrew.hill@random.studio>
2020-11-16 18:18:54 +0100

    Remove validator

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,7 +14,8 @@ for i in {1..100}
 do
     echo "Download Funda page $i"
     FILE="/root/Desktop/Funda/Koop/page$i.html"
-    /root/Desktop/savePages.sh "https://www.funda.nl/koop/1012xa/350000-450000/50+woonopp/2+kamers/+3km/p$i/" -b "firefox" -d "$FILE"
+    source /root/Desktop/savePages.sh "https://www.funda.nl/koop/1012xa/350000-450000/50+woonopp/2+kamers/+3km/p$i/" -b "firefox" -d "$FILE"
+    wait
     if grep -q "Kies een groter gebied" "$FILE"; then
         break
     fi
@@ -35,22 +36,8 @@ echo "Geocoding json files ..."
 nodejs /root/Desktop/geocode.js
 nodejs /root/Desktop/addMissingCoords.js
 sleep 5s
-VALID=$(nodejs /root/Desktop/validate.js)
-sleep 5s
 
-if [ "$VALID" == "true" ]; then
-    echo "Geocoding finished ..."
-
-    cp /root/Desktop/teKoop.json /home/meteorapp/build/bundle/programs/web.browser/app/data/teKoop.json
-else
-    echo "Some errors occured while geocoding ..."
-    echo "Not saving results ..."
-fi
-
-echo "------------------------------"
-echo "Cleaning up old files ..."
-
-rm /root/Desktop/teKoop*.json
+echo "Geocoding finished ..."
 
 if [ -d /root/Desktop/Funda ]; then
     rm -rf /root/Desktop/Funda
